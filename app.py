a37dbb9fbc3e285d6779bfaa38411d91ee4f17fc
AJAY KUMAR <143438500+ajay-navodayan@users.noreply.github.com>
2024-09-03 21:49:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -648,5 +648,6 @@ def schedule_emails():
 
 if __name__ == '__main__':
     threading.Thread(target=schedule_emails, daemon=True).start()
-    port = int(os.environ.get('PORT'), 5000)
+    port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+

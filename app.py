0416b9fdcdb2f0f071aa79d5bc70ad83f48339cc
AJAY KUMAR <143438500+ajay-navodayan@users.noreply.github.com>
2024-09-03 22:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ google = oauth.register(
     access_token_params=None,
     refresh_token_url=None,
     refresh_token_params=None,
-    redirect_uri='http://127.0.0.1:5000/authorize',
+    redirect_uri='https://feedback-management-system-my6t.onrender.com/authorize',
     client_kwargs={'scope': 'openid email profile'},
     jwks_uri='https://www.googleapis.com/oauth2/v3/certs',
 )

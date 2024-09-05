b6796dd4e9283c3425fa401727249444d33a56ab
AJAY KUMAR <143438500+ajay-navodayan@users.noreply.github.com>
2024-09-05 11:29:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def authorize():
 
         if re.match(r'^su-.*@sitare\.org$', email):
             return redirect(url_for('dashboard'))
-        elif re.match(r'^(kpuneet474@gmail\.com|kushal@sitare\.org|preeti@sitare\.org|ajaynavodayan01@gmail.com)$', user_info['email']):
+        elif re.match(r'^(kpuneet474@gmail\.com|kushal@sitare\.org|preeti@sitare\.org|ajaynavodayan01@gmai\l.com)$', user_info['email']):
             return redirect(url_for('teacher_portal'))
         elif re.match(r'^krishu747@gmail\.com$', email):
             return redirect(url_for('admin_portal'))
@@ -122,7 +122,7 @@ def dashboard():
 
     if re.match(r'^su-.*@sitare\.org$', user_info['email']):
         return redirect(url_for('student_portal'))
-    elif re.match(r'^(kpuneet474@gmail\.com|kushal@sitare\.org|preeti@sitare\.org|ajaynavodayan01@gmail.com)$', user_info['email']):
+    elif re.match(r'^(kpuneet474@gmail\.com|kushal@sitare\.org|preeti@sitare\.org|ajaynavodayan01@gmail\.com)$', user_info['email']):
         return redirect(url_for('teacher_portal'))
     elif re.match(r'^krishu747@gmail\.com$', user_info['email']):
         return redirect(url_for('admin_portal'))
